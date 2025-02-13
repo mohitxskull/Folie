3480f9e5989f848260163d7bc09c76c122ac7042
mohitxskull <mohitxskull@gmail.com>
2025-02-13 16:21:58 +0530

    wip: removed dishook package

--- packages/dishook/eslint.config.js
+++
@@ -1,9 +0,0 @@
-import { configApp } from '@adonisjs/eslint-config'
-
-export default configApp({
-  languageOptions: {
-    parserOptions: {
-      warnOnUnsupportedTypeScriptVersion: false,
-    },
-  },
-})
